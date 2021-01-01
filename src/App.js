ab6b1cda17ce402ffa6765a572e2140da158bcc8
Sarthak Agarwal (Accenture Services Private Ltd) <v-agsart@microsoft.com>
2021-01-02 00:36:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import test from './test.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={test} alt="test" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
