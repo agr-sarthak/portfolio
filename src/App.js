ec37087302f1c1dabb7d8550717047222ec0f554
Sarthak Agarwal (Accenture Services Private Ltd) <v-agsart@microsoft.com>
2021-01-02 18:42:59 +0530

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import test from './test.jpg'
+import Game from './components/Game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="header">
+      <div>
+        <div className="textCenter">
+          Hi!! Welcome to My React Website
+        </div>
+        <br />
+        Let's have a game of tic-tac-toe
+        <Game />
+      </div>
+
+      <div>
+        <div>Thank you for visiting</div>
         <div className="info">
-          <img src={test} alt="test" className="myImage"/>
+          <img src={test} alt="test" className="myImage" />
           <div>
-            <b>Sarthak Agarwal</b><br/>
-            Senior Software Engineer<br/>
+            <b>Sarthak Agarwal</b><br />
+            Senior Software Engineer<br />
             Accenture Solutions Pvt. Ltd.
           </div>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
